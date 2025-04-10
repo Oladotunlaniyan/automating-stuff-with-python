d6b1755e2b5e63000010656b7e0b4ea41fd8826f
Abdul-Qawi Laniyan <oladotunlaniyan@gmail.com>
2025-04-10 22:48:38 +0100

    Chapter 10 first part

---
+++ Chapter 10/hello.py
@@ -0,0 +1,27 @@
+import shutil, os, zipfile
+from pathlib import Path
+# shutil.copy(source, destination)
+
+
+
+# p = Path.home()
+
+# shutil.copytree(p/'spam', p/'spam_backup')
+
+# shutil.move('C:\\bacon.txt', 'C:\\eggs')
+# shutil.move('C:\\bacon.txt', 'C:\\eggs')
+
+# for folderName, subfolders, filenames in os.walk('C:\\delicious'):
+#     print('The current folder is ' + folderName)
+
+#     for subfolder in subfolders:
+#         print('SUBFOLDER OF' + folderName + ': ' + subfolder)
+
+# from pathlib import Path
+
+# p = Path.home()
+# exampleZip = zipfile.ZipFile(p/'example.zip')
+# exampleZip.namelist()
+# spamInfo = exampleZip.getinfo('spam.txt')
+
+
