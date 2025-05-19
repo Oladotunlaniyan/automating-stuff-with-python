ea5c18357a99700796e80aeb5bb45f197014b85f
Abdul-Qawi Laniyan <oladotunlaniyan@gmail.com>
2025-05-19 12:19:52 +0100

    Chapter 18

--- Chapter 16/randomChores.py
+++ Chapter 16/randomChores.py
@@ -1,21 +1,4 @@
-# # import csv 
-# # exampleFile = open('example.csv')
-# # exampleReader = csv.reader(exampleFile)
-# # exampleData = list(exampleReader)
-# # exampleData 
-
-# # exampleData[0][0]
-# # exampleData[0][1]
-# # exampleData[1][1]
-# # exampleData[6][1]
-
-# # import csv 
-
-# # outputFile = open('output.csv', 'w', newline='')
-# # outputWriter = csv.writer(outputFile)
-# # outputWriter.writerow(['Hello, world!'])
-
-# """Scans emails for unsubscribe links and opens them in a browser."""
+# # Scans emails for unsubscribe links and opens them in a browser.
 
 # import webbrowser
 # import imaplib
